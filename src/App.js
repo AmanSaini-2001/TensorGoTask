3b76ca6d1ef79de70f6d638b6c015cbc0500628e
Aman Saini <amansaini50016@gmail.com>
2023-12-04 17:14:11 +0530

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import InvoiceList from './components/InvoiceList';
+import AutomationButton from './components/AutomationButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SaaS Platform</h1>
+      <GoogleLogin
+        clientId="YOUR_GOOGLE_CLIENT_ID"
+        buttonText="Login with Google"
+        onSuccess={(response) => console.log(response)}
+        onFailure={(error) => console.error(error)}
+        cookiePolicy={'single_host_origin'}
+      />
+      <InvoiceList />
+      <AutomationButton />
     </div>
   );
 }
